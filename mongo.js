bbd1ceb1d9504dc5e282c79dde4d8265f7367d57
LucasCasa <lcasagrande@itba.edu.ar>
2018-10-14 13:05:19 -0300

    mongo driver, some api to get near

---
+++ mongo.js
@@ -0,0 +1,46 @@
+var db;
+
+function findNear(table, lat, long, dist, callback) {
+    // Get the documents collection
+    const collection = db.collection(table);
+    // Find some documents
+    collection.find(
+        { 'geometry':
+                { $near :
+                        { $geometry:
+                                { type: "Point",  coordinates: [ parseFloat(long), parseFloat(lat) ] },
+                            $maxDistance: parseFloat(dist)
+                        }
+                }
+        }
+    ).toArray(function(err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs);
+        callback(docs);
+    });
+}
+
+// use the findDocuments() function
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert')
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'sube';
+
+const client = new MongoClient(url);
+
+// Use connect method to connect to the Server
+client.connect(function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+
+    db = client.db(dbName);
+});
+
+module.exports = {
+  findNear: findNear,
+    mongobd: db
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const db = require('../db');
+const mongo = require('../mongo');
 
 var router = express.Router();
 
@@ -13,6 +14,19 @@ router.get('/test', function(req, res, next) {
         .then(result => {
             res.status(200).send(result);
         });
+});
+
+router.get('/mongo', function(req, res, next) {
+    mongo.find(result => {
+        res.status(200).send(result);
+    });
+});
+
+router.get('/:table/near/:lat/:long/:dist', function(req, res, next) {
+
+    mongo.findNear(req.params.table, req.params.lat, req.params.long, req.params.dist, result => {
+        res.status(200).send(result);
+    });
 });
 
 module.exports = router;
